870df7b523dd96e883c975de77fb99a0f1ec5548
Garrin Kimmell <kimmell@kestrel.edu>
2014-07-13 13:49:28 -0700

    Added first plot

---
+++ plot1.R
@@ -0,0 +1,13 @@
+
+data <- read.csv('household_power_consumption.txt', sep=';', stringsAsFactors=FALSE);                 
+data["Date"] <- lapply(data["Date"], function(d) as.Date(d, "%d/%m/%Y"))
+# data$fixed <- strptime(data$Date, data$Time, "%d/%m/%Y %H:%M%S")
+  
+targets <- as.Date(c('2007-02-01', '2007-02-02'), "%Y-%m-%d")
+
+interesting <- data[data$Date %in% targets,]
+interesting$Global_active_power <- sapply(interesting$Global_active_power, as.numeric)
+
+# Generate Plot 1
+hist(interesting$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
+
